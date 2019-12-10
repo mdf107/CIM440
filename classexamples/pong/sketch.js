0d50a03f326d0a93527e3802c37dc580bb922d49
Maxim Fisher <mdf107@miami.edu>
2019-12-10 12:45:43 -0500

    Final

--- classexamples/pong/sketch.js
+++ classexamples/pong/sketch.js
@@ -1,29 +1,21 @@
-//pong clone
-//mouse to control both paddles
 var startScreen;
 var gameScreen;
 var scoreBoard;
 var gameOver;
-
 var leftPlayer = 0;
 var rightPlayer = 0;
-
 var title = ["Welcome to Miami Vice Pong!"]
 var instruction = ["Hit P on Your Keyboard to Play"]
-var endmessage = ["Game Over! Click to Reset"]
-
+var endmessage = ["Game Over! Hit R to Reset"]
 var gameScreen = false;
 var paddleA, paddleB, ball, wallTop, wallBottom;
 var MAX_SPEED = 10;
-
 var interval = 5000;
 var prevMillis = 0;
 var counter = 0;
-
 var playeraScore = 0;
 var playerbScore = 0;
 var txtSize = 50;
-
 var reset;
 var rbutton;
 
@@ -33,7 +25,6 @@ function preload(){
 }
 
 function setup() {
-
   createCanvas(800, 400);
   startScreen = true;
 
@@ -49,28 +40,35 @@ function setup() {
   ball.maxSpeed = MAX_SPEED;
   paddleA.shapeColor = paddleB.shapeColor =ball.shapeColor = color(255, 255, 255);
   ball.setSpeed(MAX_SPEED, -180);
+  paddleB.setSpeed(MAX_SPEED, 20);
+  //paddleB.velocity.y = 5;
+
 
 }
 
 function draw() {
-  if(startScreen==true){
-  background(miamiImage);
-  textSize(60);
-  fill("white");
-  textFont('Lato');
-  textStyle(BOLDITALIC);
-  text(title, 20, 100);
-  text(instruction, 20, 180);
-  }
+    if(startScreen==true){
+      background(miamiImage);
+      textSize(60);
+      fill("white");
+      textFont('Lato');
+      textStyle(BOLDITALIC);
+      text(title, 20, 100);
+      text(instruction, 20, 180);
+    }
 
     if(gameScreen==true){
       console.log("game");
       background(courtImage);
       paddleA.position.y = constrain(mouseY, paddleA.height/2, height-paddleA.height/2);
-      paddleB.position.y = constrain(mouseY, paddleA.height/2, height-paddleA.height/2);
+      // paddleB.position.y = constrain(mouseY, paddleA.height/2, height-paddleA.height/2);
+      paddleB.position.x = width/2 + 300;
+      paddleB.bounce(wallTop);
+      paddleB.bounce(wallBottom);
       ball.bounce(wallTop);
       ball.bounce(wallBottom);
       var swing;
+
       if(ball.bounce(paddleA)) {
         swing = (ball.position.y-paddleA.position.y)/3;
         ball.setSpeed(MAX_SPEED, ball.getDirection()+swing);
@@ -79,6 +77,20 @@ function draw() {
       if(ball.bounce(paddleB)) {
         swing = (ball.position.y-paddleB.position.y)/3;
         ball.setSpeed(MAX_SPEED, ball.getDirection()-swing);
+      }
+
+      if(paddleB.bounce(wallTop)) {
+        var swing2 = 1;
+        //paddleB.setSpeed(MAX_SPEED, paddleB.getDirection()+swing2);
+        paddleB.setSpeed(MAX_SPEED, 30);
+
+      }
+
+      if(paddleB.bounce(wallBottom)) {
+        var swing2 = 1;
+        //paddleB.setSpeed(MAX_SPEED, paddleB.getDirection()-swing2);
+        paddleB.setSpeed(MAX_SPEED, -30);
+
       }
 
       if(ball.position.x<0) {
@@ -113,30 +125,38 @@ function draw() {
           ball.position.x = width/2;
           ball.position.y = height/2;
           ball.setSpeed(MAX_SPEED, 180);
+
+          paddleB.position.x = width/2 + 300;
+          paddleB.position.y = height/2;
+          paddleB.setSpeed(MAX_SPEED, 30);
         }
 
       }
 
-      if(gameOver==true){
-        background(miamiImage);
-        drawScores();
-        text("Player Left Score:", 200,150);
-        text("Player Right Score:", 200,250);
-        textSize(45);
-        textAlign(CENTER);
-        fill("white");
-        textFont('Lato');
-        textStyle(BOLDITALIC);
-        text(endmessage,400,70);
+    if(gameOver==true){
+      background(miamiImage);
+      drawScores();
+      text("Player Left Score:", 200,150);
+      text("Player Right Score:", 200,250);
+      textSize(45);
+      textAlign(CENTER);
+      fill("white");
+      textFont('Lato');
+      textStyle(BOLDITALIC);
+      text(endmessage,400,70);
       }
 
-      if(playeraScore==7){
-        gameOver = true;
+    if(playeraScore==7){
+      gameOver = true;
+      gameScreen = false;
+      scoreBoard = false;
       }
-      if(playerbScore==7){
-        gameOver = true;
+    if(playerbScore==7){
+      gameOver = true;
+      gameScreen = false;
+      scoreBoard = false;
       }
-    }
+  }
 
 function drawScores(){
   let x1 = 560;
@@ -151,14 +171,18 @@ function drawScores(){
   text(playerbScore, x2, y2);
 }
 
-  function keyPressed(){
-    if(key == 'r'){
-      startScreen = true;
-    }
-    if(key == 'p'){
-      gameScreen = true;
-      ball.position.x = width/2;
-      ball.position.y = height/2;
-    }
-
+function keyPressed(){
+  if(key == 'p'){
+    gameScreen = true;
+    ball.position.x = width/2;
+    ball.position.y = height/2;
   }
+  if(key == 'r'){
+    gameScreen = false;
+    scoreBoard = false;
+    gameOver = false;
+    playeraScore = 0;
+    playerbScore = 0;
+    startScreen = true;
+  }
+}
